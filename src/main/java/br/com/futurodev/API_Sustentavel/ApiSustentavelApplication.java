e51a10f03dffb51d9de620f45199e4446deaf04d
Marcos Vinicius Santos da Silva <marcos_viny_12@outlook.com>
2025-04-17 19:42:14 -0300

    add: arquivos iniciais do projeto

---
+++ src/main/java/br/com/futurodev/API_Sustentavel/ApiSustentavelApplication.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.API_Sustentavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSustentavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSustentavelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/futurodev/API_Sustentavel/ApiSustentavelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.futurodev.API_Sustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSustentavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
